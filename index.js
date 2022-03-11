186f2e2859832d2f9b0dc5057bcf3cdd8bd06c26
ra-mariano <ronan.a.mariano@gmail.com>
2022-03-11 15:26:43 -0500

    basic functionality including add items, clear items now working

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 document.getElementById("onebutton").addEventListener("click", oneButton);
+document.getElementById("clearone").addEventListener("click", clearOne);
+document.getElementById("twobutton").addEventListener("click", twoButton);
+document.getElementById("cleartwo").addEventListener("click", clearTwo);
 
 function oneButton() {
   let productoneuserquantity = document.getElementById("inputone").value
   let productonedisplayedquantity =  document.getElementById("onequantity") //The quantity row
   let productone = document.getElementById("productonebox")
+  let producttwodisplayedquantity =  document.getElementById("twoquantity")
+  let producttwo = document.getElementById("producttwobox")
   let total =   document.getElementById("total")
 
 
@@ -20,7 +25,63 @@ function oneButton() {
  
  //productonedisplayedquantity.setAttribute("value", productoneuserquantity) // string
 
-total.innerHTML = "$" + parseInt(productone.dataset.price*productoneuserquantity) + ".00"
+ total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+".00")
+//total.innerHTML = "$" + parseInt(productone.dataset.price*productonedisplayedquantity) + (producttwo.dataset.price*producttwodisplayedquantity) +".00"
+
+} 
+
+function clearOne() {
+  document.getElementById("onequantity").innerHTML = 0
+  document.getElementById("onequantity").setAttribute("value", 0)
+
+  let productone = document.getElementById("productonebox")
+  let producttwo = document.getElementById("producttwobox")
+  let productonedisplayedquantity = document.getElementById("onequantity")
+  let producttwodisplayedquantity =  document.getElementById("twoquantity") //The quantity row
+  let total =   document.getElementById("total")
+
+  total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+".00")
+}
+
+function twoButton() {
+  let productonedisplayedquantity =  document.getElementById("onequantity") //The quantity row
+  let productone = document.getElementById("productonebox")
+  let producttwouserquantity = document.getElementById("inputtwo").value
+  let producttwodisplayedquantity =  document.getElementById("twoquantity") //The quantity row
+  let producttwo = document.getElementById("producttwobox")
+  let total =   document.getElementById("total")
+
+
+//console.log(document.getElementById("twoquantity").getAttribute("value"))
+  // console.log(document.getElementById("inputtwo").value)
+  // console.log(productquantity)
+  console.log(producttwodisplayedquantity.getAttribute("value"))
+  
+ producttwodisplayedquantity.innerHTML = parseInt(producttwouserquantity) + parseInt(producttwodisplayedquantity.getAttribute("value")) // number
+ producttwodisplayedquantity.setAttribute("value", parseInt(producttwodisplayedquantity.innerHTML)) 
+
+ document.getElementById("inputtwo").value=0
+ 
+ //producttwodisplayedquantity.setAttribute("value", producttwouserquantity) // string
+
+ total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+".00")
 
 
 } 
+
+function clearTwo() {
+  document.getElementById("twoquantity").innerHTML = 0
+  document.getElementById("twoquantity").setAttribute("value", 0)
+
+  let productone = document.getElementById("productonebox")
+  let producttwo = document.getElementById("producttwobox")
+  let productonedisplayedquantity = document.getElementById("onequantity")
+  let producttwodisplayedquantity =  document.getElementById("twoquantity") //The quantity row
+  let total =   document.getElementById("total")
+
+// producttwodisplayedquantity.innerHTML = parseInt(producttwouserquantity) + parseInt(producttwodisplayedquantity.getAttribute("value")) // number
+// producttwodisplayedquantity.setAttribute("value", parseInt(producttwodisplayedquantity.innerHTML)) 
+
+
+  total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+".00")
+}
