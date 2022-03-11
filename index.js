2e4f1889575416fb38c2d731aeff8855512a4818
ra-mariano <ronan.a.mariano@gmail.com>
2022-03-11 01:29:55 -0500

    got logic down for adding subsequent quantities

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-console.log("working?")
 
 document.getElementById("onebutton").addEventListener("click", oneButton);
 
 function oneButton() {
-  console.log(document.getElementById("inputone").value)
-  console.log(document.getElementById("onequantity").value)
-  document.getElementById("onequantity").innerHTML = document.getElementById("inputone").value
+  let productoneuserquantity = document.getElementById("inputone").value
+  let productonedisplayedquantity =  document.getElementById("onequantity") //The quantity row
+  let productone = document.getElementById("productonebox")
+  let total =   document.getElementById("total")
+
+
+//console.log(document.getElementById("onequantity").getAttribute("value"))
+  // console.log(document.getElementById("inputone").value)
+  // console.log(productquantity)
+  console.log(productonedisplayedquantity.getAttribute("value"))
+  
+ productonedisplayedquantity.innerHTML = parseInt(productoneuserquantity) + parseInt(productonedisplayedquantity.getAttribute("value")) // number
+ productonedisplayedquantity.setAttribute("value", parseInt(productonedisplayedquantity.innerHTML)) 
+
+ document.getElementById("inputone").value=0
+ 
+ //productonedisplayedquantity.setAttribute("value", productoneuserquantity) // string
+
+total.innerHTML = "$" + parseInt(productone.dataset.price*productoneuserquantity) + ".00"
+
+
 } 
