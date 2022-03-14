244d99ffb0485e56cd3b618733ad77cb3f24aa5f
ra-mariano <ronan.a.mariano@gmail.com>
2022-03-14 02:31:35 -0400

    working functionality for one-time payments and started on Basic Package for subscription payments

--- server/public/index.js
+++ server/public/index.js
@@ -3,6 +3,11 @@ document.getElementById("onebutton").addEventListener("click", oneButton);
 document.getElementById("clearone").addEventListener("click", clearOne);
 document.getElementById("twobutton").addEventListener("click", twoButton);
 document.getElementById("cleartwo").addEventListener("click", clearTwo);
+document.getElementById("threebutton").addEventListener("click", threeButton);
+// document.getElementById("threebutton").addEventListener("click", logSubChoice);
+
+let items = []
+
 
 function oneButton() {
   let productoneuserquantity = document.getElementById("inputone").value
@@ -11,35 +16,37 @@ function oneButton() {
   let producttwodisplayedquantity =  document.getElementById("twoquantity")
   let producttwo = document.getElementById("producttwobox")
   let total =   document.getElementById("total")
-
-
-//console.log(document.getElementById("onequantity").getAttribute("value"))
+  
+  
+  //console.log(document.getElementById("onequantity").getAttribute("value"))
   // console.log(document.getElementById("inputone").value)
   // console.log(productquantity)
   console.log(productonedisplayedquantity.getAttribute("value"))
   
- productonedisplayedquantity.innerHTML = parseInt(productoneuserquantity) + parseInt(productonedisplayedquantity.getAttribute("value")) // number
- productonedisplayedquantity.setAttribute("value", parseInt(productonedisplayedquantity.innerHTML)) 
-
- document.getElementById("inputone").value=0
- 
- //productonedisplayedquantity.setAttribute("value", productoneuserquantity) // string
-
- total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+".00")
-//total.innerHTML = "$" + parseInt(productone.dataset.price*productonedisplayedquantity) + (producttwo.dataset.price*producttwodisplayedquantity) +".00"
-
+  productonedisplayedquantity.innerHTML = parseInt(productoneuserquantity) + parseInt(productonedisplayedquantity.getAttribute("value")) // number
+  productonedisplayedquantity.setAttribute("value", parseInt(productonedisplayedquantity.innerHTML)) 
+  
+  document.getElementById("inputone").value=0
+  
+  //productonedisplayedquantity.setAttribute("value", productoneuserquantity) // string
+  
+  total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+".00")
+  //total.innerHTML = "$" + parseInt(productone.dataset.price*productonedisplayedquantity) + (producttwo.dataset.price*producttwodisplayedquantity) +".00"
+  
+  
+  
 } 
 
 function clearOne() {
   document.getElementById("onequantity").innerHTML = 0
   document.getElementById("onequantity").setAttribute("value", 0)
-
+  
   let productone = document.getElementById("productonebox")
   let producttwo = document.getElementById("producttwobox")
   let productonedisplayedquantity = document.getElementById("onequantity")
   let producttwodisplayedquantity =  document.getElementById("twoquantity") //The quantity row
   let total =   document.getElementById("total")
-
+  
   total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+".00")
 }
 
@@ -50,64 +57,116 @@ function twoButton() {
   let producttwodisplayedquantity =  document.getElementById("twoquantity") //The quantity row
   let producttwo = document.getElementById("producttwobox")
   let total =   document.getElementById("total")
-
-
-//console.log(document.getElementById("twoquantity").getAttribute("value"))
+  
+  //console.log(document.getElementById("twoquantity").getAttribute("value"))
   // console.log(document.getElementById("inputtwo").value)
   // console.log(productquantity)
   console.log(producttwodisplayedquantity.getAttribute("value"))
   
- producttwodisplayedquantity.innerHTML = parseInt(producttwouserquantity) + parseInt(producttwodisplayedquantity.getAttribute("value")) // number
- producttwodisplayedquantity.setAttribute("value", parseInt(producttwodisplayedquantity.innerHTML)) 
-
- document.getElementById("inputtwo").value=0
- 
- //producttwodisplayedquantity.setAttribute("value", producttwouserquantity) // string
-
- total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+".00")
-
-
+  producttwodisplayedquantity.innerHTML = parseInt(producttwouserquantity) + parseInt(producttwodisplayedquantity.getAttribute("value")) // number
+  producttwodisplayedquantity.setAttribute("value", parseInt(producttwodisplayedquantity.innerHTML)) 
+  
+  document.getElementById("inputtwo").value=0
+  
+  //producttwodisplayedquantity.setAttribute("value", producttwouserquantity) // string
+  
+  total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+".00")
+  
 } 
 
 function clearTwo() {
   document.getElementById("twoquantity").innerHTML = 0
   document.getElementById("twoquantity").setAttribute("value", 0)
-
+  
   let productone = document.getElementById("productonebox")
   let producttwo = document.getElementById("producttwobox")
   let productonedisplayedquantity = document.getElementById("onequantity")
   let producttwodisplayedquantity =  document.getElementById("twoquantity") //The quantity row
   let total =   document.getElementById("total")
-
-// producttwodisplayedquantity.innerHTML = parseInt(producttwouserquantity) + parseInt(producttwodisplayedquantity.getAttribute("value")) // number
-// producttwodisplayedquantity.setAttribute("value", parseInt(producttwodisplayedquantity.innerHTML)) 
-
-
+  
+  // producttwodisplayedquantity.innerHTML = parseInt(producttwouserquantity) + parseInt(producttwodisplayedquantity.getAttribute("value")) // number
+  // producttwodisplayedquantity.setAttribute("value", parseInt(producttwodisplayedquantity.innerHTML)) 
+  
   total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+".00")
 }
 
-const button = document.getElementById("checkoutbutton")
-button.addEventListener("click", () => {
-  console.log("working")
-  fetch("/create-checkout-session", {
+function threeButton() {
+
+  let productonedisplayedquantity =  document.getElementById("onequantity") //The quantity row
+  let productone = document.getElementById("productonebox")
+  let producttwodisplayedquantity =  document.getElementById("twoquantity") //The quantity row
+  let producttwo = document.getElementById("producttwobox")
+
+  if (document.getElementById("dropdown").value=="Basic Package") {
+  //let productthreeuserquantity = document.getElementById("inputthree").value
+  let productthreedisplayedquantity =  document.getElementById("threequantity") //The quantity row
+  let total =   document.getElementById("total")
+
+  productthreedisplayedquantity.innerHTML= 1
+  productthreedisplayedquantity.setAttribute("value", 1)
+
+
+   
+  total.innerHTML = "$".concat(parseInt(productone.dataset.price)*parseInt(productonedisplayedquantity.getAttribute("value")) + (parseInt(producttwo.dataset.price)*parseInt(producttwodisplayedquantity.getAttribute("value")))+12+".00")
+  
+} 
+}
+
+const checkoutbutton = document.getElementById("checkoutbutton")
+checkoutbutton.addEventListener("click", () => {
+  
+  if(parseInt(document.getElementById("twoquantity").getAttribute("value"))>0) {
+    items.push( { id: 2, quantity: parseInt(document.getElementById("twoquantity").getAttribute("value")) })
+  }
+  if(parseInt(document.getElementById("onequantity").getAttribute("value"))>0) {
+    items.push({ id: 1, quantity: parseInt(document.getElementById("onequantity").getAttribute("value")) })
+  }
+  
+  document.getElementById("twoquantity").setAttribute("value",0)
+  
+  fetch("/checkout-page", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      items: [
-        { id: 1, quantity: parseInt(document.getElementById("onequantity").getAttribute("value")) },
-        { id: 2, quantity: parseInt(document.getElementById("twoquantity").getAttribute("value")) },
-      ],
+      items
     }),
   })
-  .then(res => {
-    if (res.ok) return res.json()
-    return res.json().then(json =>Promise.reject(json))
+  .then(response => {
+    if (response.ok) return response.json()
+    return response.json().then(json =>Promise.reject(json))
   })
-  .then(({ url}) => {
+  .then(({ url }) => {
     window.location = url
-  }).catch(e => {
-    console.error(e.error)
   })
+  .catch(e => {
+    //console.error(e.error)  the original
+    //console.log("oh my!")   this works but doesn't indicate error
+    //console.error(e)        outputs same as console.log(e), unsure when to use which exactly
+    console.log(e)
+    
+    
+  });
+  
+    
 })
+
+// // function logSubChoice() {
+// //   console.log("working?")
+// //   console.log(document.getElementById("dropdown").value)
+  
+// };
+
+window.addEventListener('pageshow', function(event) {
+  if (event.persisted) {
+    console.log('Page was loaded from cache.');
+    
+//     document.getElementById("twoquantity").innerHTML = 0
+//   document.getElementById("twoquantity").setAttribute("value", 0)
+//   document.getElementById("onequantity").innerHTML = 0
+// document.getElementById("onequantity").setAttribute("value",0)
+// total.innerHTML = "$0.00"
+window.location.reload()
+  }
+});
--- server/server.js
+++ server/server.js
@@ -8,37 +8,86 @@ app.use(express.static("public"))
 
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
+// document.getElementById("threebutton").addEventListener("click", logSubChoice);
+
+// function logSubChoice() {
+//   console.log("working?")
+// console.log(document.getElementById("dropdown").value)
+// };
+
+
+
+
 const storeItems = new Map([
-  [1, {priceInCents: 2500, name: "Test Product One"}],
-  [2, {priceInCents: 1500, name: "Test Product Two"}]
+  [1, {priceID: "price_1KbpENHBuM9OaL8xPeeVFAa8", name: "Test Product One", }],
+  [2, {priceID: "price_1KbpExHBuM9OaL8xjgRtxqGY", name: "Test Product Two"}],
+  
 ])
 
-app.post('/create-checkout-session', async (req, res) => {
-  try {
-  const session = await stripe.checkout.sessions.create({
-    line_items: req.body.items.map(item=> {
-      const storeItem = storeItems.get(item.id)
-      return {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: storeItem.name
-          },
-          unit_amount: storeItem.priceInCents
-        },
-        quantity: item.quantity 
-      }
-    }),
-    mode: 'payment',
-    success_url: "http://127.0.0.1:4242",
-    cancel_url: 'https://example.com/cancel',
-  })
-  res.json({ url: session.url })
-  }
-  catch (e) {
-    res.status(500).json({ error: e.message })
-  }
+app.post('/checkout-page-sub', async (req, res) => {
   
-});
+  
+  
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {price: "price_1JktlkHBuM9OaL8xrMMEnF62", quantity: 1}],
+      mode: 'subscription',
+      success_url: "https://google.com",
+      cancel_url: 'https://example.com/cancel',
+    });
+    
+    res.redirect(303, session.url);
+    
+  });
+  
+  app.post('/checkout-page', async (req, res) => {
+    try {
+      const session = await stripe.checkout.sessions.create({
+        line_items: req.body.items.map(item=> {
+          const storeItem = storeItems.get(item.id)
+          // return {
+            //   price_data: {
+              //     currency: "usd",
+              //     product_data: {
+                //       name: storeItem.name
+                //     },
+                //     unit_amount: storeItem.priceInCents
+                //   },
+                //   quantity: item.quantity 
+                // }
+                return {
+                  price: storeItem.priceID,
+                  quantity: item.quantity
+                }
+              }),
+              mode: 'payment',
+              success_url: "https://google.com",
+              cancel_url: 'https://example.com/cancel',
+            })
+            res.json({ url: session.url })
+            // res.redirect(303, session.url);
+            
+          }
+          catch (e) {
+            res.status(500).json({ error: e.message })
+          }
+          
+        });
+        
+        
+        app.listen(4242, () => console.log(`Listening on port ${4242}!`));
 
-app.listen(4242, () => console.log(`Listening on port ${4242}!`));
+
+
+
+
+
+
+
+
+
+        
+        // const storeItems = new Map([
+        //   [1, {priceInCents: 2500, name: "Test Product One", }],
+        //   [2, {priceInCents: 1500, name: "Test Product Two"}]
+        // ])
